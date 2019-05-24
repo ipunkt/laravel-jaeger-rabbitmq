9cee9953bfeaa0177f0824f7927c0061e904370d
Sven Speckmaier <sps@ipunkt.biz>
2019-05-24 23:11:58 +0200

    added basic test setup

---
+++ tests/TestCase.php
@@ -0,0 +1,9 @@
+<?php namespace Ipunkt\LaravelJaegerRabbitMQTests;
+
+/**
+ * Class TestCase
+ * @package Ipunkt\LaravelJaegerRabbitMQTests
+ */
+class TestCase extends \PHPUnit\Framework\TestCase {
+
+}
