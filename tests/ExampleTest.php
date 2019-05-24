2b781e1bb22823ff0c20aa970867f29da4dc1e6c
Sven Speckmaier <sps@ipunkt.biz>
2019-05-24 23:14:35 +0200

    example test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php namespace Ipunkt\LaravelJaegerRabbitMQTests;
+
+/**
+ * Class ExampleTest
+ * @package Ipunkt\LaravelJaegerRabbitMQTests
+ */
+class ExampleTest extends TestCase {
+
+	/**
+	 * @test
+	 */
+	public function example(  ) {
+		$this->assertTrue(true);
+	}
+
+}
